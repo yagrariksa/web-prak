96782ead4df39b6c1054ac20e8b0d7f6b1c833d1
yagrariksa <luckyrama0@gmail.com>
2021-03-25 22:27:42 +0700

    initial

---
+++ php/admin-login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+
+<body>
+
+    <a href="index.php">Home</a>
+    <h1>
+        admin login
+    </h1>
+
+    <form action="">
+        <input type="text" name="" id="" placeholder="username"> <br>
+        <input type="password" name="" id="" placeholder="password"> <br>
+        <button type="submit">Log In</button>
+    </form>
+</body>
+
+</html>
---
+++ php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PemWEB</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <h5>Daffa Yagrariksa & Helmi Fachry</h5>
+
+    <a href="/admin-login.php">Admin Login</a>
+</body>
+</html>
